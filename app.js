9221dc737f397d572ba70c1e50287fe38ae88877
William Levack Payne <108789355+WillLP-code@users.noreply.github.com>
2024-03-11 12:30:04 +0000

    input sent to pyodide, no conversion yet

--- app.js
+++ app.js
@@ -1,27 +1,25 @@
-(function () {
-
-    // var input = document.getElementById('file');
-    var input = document.getElementById('file');
-    var table = document.getElementById('table');
-
-    if (!input) {
-        return;
-    }
-
-    input.addEventListener('change', function() {
-        if (!!input.files && input.files.length > 0) {
-            // implement toJSON() behavior  
-            var input = input.files[0]
-            input.toJSON = function() { return {
-                'lastModified'     : myFile.lastModified,
-                'lastModifiedDate' : myFile.lastModifiedDate,
-                'name'             : myFile.name,
-                'size'             : myFile.size,
-                'type'             : myFile.type 
-            };}  
-            
-            // then use JSON.stringify on File object
-            input.stringify(input);
+    function getFiles(){
+        var files = document.getElementById("file");
+        var myArray = [];
+        var file = {};
+    
+        console.log(files); // see the FileList
+    
+        // manually create a new file obj for each File in the FileList
+        for(var i = 0; i < files.length; i++){
+    
+          file = {
+              'lastMod'    : files[i].lastModified,
+              'lastModDate': files[i].lastModifiedDate,
+              'name'       : files[i].name,
+              'size'       : files[i].size,
+              'type'       : files[i].type,
+          } 
+    
+          //add the file obj to your array
+          myArray.push(file)
         }
-    });
-})();
+    
+        //stringify array
+        console.log(JSON.stringify(myArray));
+    }
