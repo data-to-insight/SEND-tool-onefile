1c71d1b78a7aa562905f93335f715d28f15d41ef
William Levack Payne <108789355+WillLP-code@users.noreply.github.com>
2024-03-20 15:32:27 +0000

    fixed assessments pie

--- python_app.py
+++ python_app.py
@@ -123,8 +123,7 @@ assessment_outcome_plot = px.pie(
     modules["m3"]["Assessment Outcome To Issue EHCP"][
         modules["m3"]["Assessment Outcome To Issue EHCP"] != "H"
     ],
-    values=modules["m3"]["Assessment Outcome To Issue EHCP"].value_counts(),
-    names="Assessment Outcome To Issue EHCP"
+    values=modules["m3"]["Assessment Outcome To Issue EHCP"].value_counts()
 )
 assessment_outcome_plot = assessment_outcome_plot.to_html(
     include_plotlyjs=False, full_html=False, default_height="350px"
