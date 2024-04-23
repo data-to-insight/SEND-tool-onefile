cf2794181c9de8f8384fc33b2d98e87697429b42
William Levack Payne <108789355+WillLP-code@users.noreply.github.com>
2024-04-23 08:13:17 +0000

    added warnings package to python

--- python_app.py
+++ python_app.py
@@ -14,6 +14,7 @@ import plotly.express as px
 import plotly.graph_objects as go
 import numpy as np
 
+import warnings
 from pandas.core.common import SettingWithCopyWarning
 
 warnings.simplefilter(action="ignore", category=SettingWithCopyWarning)
